5a318868a953c3c3456bb065c4dc48a0aa5e2c57
unknown <kumar.ankit383@gmail.com>
2020-12-04 14:09:57 +0530

    Minor changes in the dashboard

--- src/containers/Transaction.js
+++ src/containers/Transaction.js
@@ -3,6 +3,26 @@ import Loader from '../components/Loader';
 import { firebase } from "../firebase/firebase";
 import { Doughnut } from 'react-chartjs-2';
 
+
+function getMonthDay(month) {
+    switch(month) {
+        case 1 : return 31;
+        case 2 : return 28;
+        case 3 : return 31;
+        case 4 : return 30;
+        case 5 : return 31;
+        case 6 : return 30;
+        case 7 : return 31;
+        case 8 : return 31;
+        case 9 : return 30;
+        case 10 : return 31;
+        case 11 : return 30;
+        case 12 : return 31;
+        default : return null;
+    }
+} 
+
+
 class Transaction extends Component {
 
     constructor(props) { 
@@ -77,12 +97,15 @@ class Transaction extends Component {
                     total_income
                 ],
                 backgroundColor : [
-                    'lightgreen',
-                    'rgba(255,99,132,0.6)'
+                    'rgba(255,99,132,0.6)',
+                    'lightgreen'                   
                 ]
             }],
             
         }
+
+        let moneyLeftToSpend = total_income - total_expense;
+        let perdayExpense = moneyLeftToSpend/(getMonthDay(new Date().getMonth() - new Date().getDay()))
 
         return (
             <div className="container">
@@ -114,11 +137,20 @@ class Transaction extends Component {
                         </table>
                     </div>
 
-                    <div className="col-sm-4">
+                    <div className="col-sm-4" style={{textAlign:"center"}}>
                         <Doughnut
                         data={data}
                         width={200}
                         />
+                        <p style={{marginTop : "20px"}}>
+                            
+                        </p>
+                        <h3>
+                            You have Rs. {moneyLeftToSpend} left in total to spend.
+                        </h3>
+                        <p>
+                            <b>{perdayExpense <= 0 ? 'You can not spend any amount further. You are already in debt.' : `You can spend Rs. ${perdayExpense} per day.`}</b>
+                        </p>
                     </div>
                 </div>
             </div>
